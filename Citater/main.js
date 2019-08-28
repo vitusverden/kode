673c39d2c284f9510f88839aa1084e50537b5788
vitusverden <37027595+vitusverden@users.noreply.github.com>
2019-08-28 15:31:15 +0200

    + citater

--- Citater/main.js
+++ Citater/main.js
@@ -52,7 +52,9 @@
  "15-08-2019 Vitus: Jaidouzeans (udtalet [chaidusang])",
  "15-08-2019 Elton (en klassekammerat): (lugter til en squishy) Ad den stinker... Den er giftig!",
  "16-08-2019 Benjamin: Halvejs igennem hoppet, huskede jeg at jeg kun havde 1 liv...",
- "16-08-2019 "
+ "20-08-2019 Lili-Luna: Kan i ikke lige have sex et andet sted?",
+ "26-06-2019 Alfred: Man skulle forfanden tro at du står i ledtog med Dansk Folkeparti.",
+ "26-06-2019 Emil: Fucking lige frem"
 ]
 window.onload=function() {
 for (let i = 0; i < citater.length; i++) {
@@ -64,7 +66,6 @@ for (let i = 0; i < citater.length; i++) {
 	} else {
 		document.getElementById("citater").innerHTML += "<p>" + "\"" + res + "\"" + "</p>"
 	}
-	console.log("å")
 
 }
 
