62aad9400d1a5868c4f3b4235b93cc40ed10fd48
vitusverden <vitu0259@gmail.com>
2019-09-26 21:15:10 +0200

    node.js stuff med benjamin gider ik forklare
    det virker ok

--- codingpirates 2019 sæson 2/nodemedbenjamin/index.js
+++ codingpirates 2019 sæson 2/nodemedbenjamin/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+const fs = require("fs")
 app.listen(8080, () => {
 	console.log("server startet")
 });
@@ -8,15 +8,78 @@ app.listen(8080, () => {
 app.use(express.urlencoded({extended:true}))
 
 app.get('/', (req, res) => {
-	res.render("index.pug")
+	res.render("index.pug", {
+		error: ""
+	})
 });
 
-app.post("/", (req, res) => {
+app.post("/register", (req, res) => {
 	console.log(req.body)
-	res.render("minside.pug", {
-		brugernavn: req.body.username,
-		password: req.body.password
+	var thing = req.body.username + " " + req.body.password
+	var x = true;
+	var name = req.body.username
+	fs.readFile('test.txt', (err, data) => {
+		if (err) throw err;
+		var logins = String(data).split("\n")
+		for (let i = 0; i < logins.length; i++) {
+			const element = logins[i];
+			if (element.includes(name)) {
+				x = false
+				console.log(element)
+			}
+		}
+		if (x) {
+			fs.readFile('test.txt', (err, data) => {
+				if (err) throw err;
+				var dataToWrite = data + "\n" + thing
+				fs.writeFile('test.txt', dataToWrite, (err) => {
+					// throws an error, you could also catch it here
+					if (err) throw err;
+				
+					// success case, the file was saved
+					console.log('Stuff saved!');
+		
+					res.render("minside.pug", {
+						brugernavn: req.body.username,
+					})
+				});
+			});
+		} else {
+			res.render("index.pug", {
+				error: "Dette account eksisterer allerede."
+			})
+		}
 	})
+
+	
+		
+
+	
+
+
+	
+});
+
+app.post("/login", (req, res) => {
+	var name = req.body.username
+	fs.readFile('test.txt', (err, data) => {
+		if (err) throw err;
+		var logins = String(data).split("\n")
+		for (let i = 0; i < logins.length; i++) {
+			const element = logins[i];
+			if (element.includes(name)) {
+				if (req.body.password == element.replace(name + " ", "")) {
+					res.render("minside.pug", {
+						brugernavn: req.body.username
+					})
+				} else {
+					res.render("index.pug", {
+						error: "Ugyldigt brugernavn eller password"
+					})
+				}
+			}
+		}
+	});
 });
 
 app.set("view engine", "pug")
